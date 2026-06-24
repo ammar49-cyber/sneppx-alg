dff6e6febc62ae37d1912a66253f9144a6ec40b1
ARIX-Algo <algoarix@gmail.com>
2026-06-24 11:53:25 -0700

    Add .gitignore, remove build artifacts from tracking

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/PC/ARIX/ARIX_Algo
-# Build directory: C:/Users/PC/ARIX/ARIX_Algo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
